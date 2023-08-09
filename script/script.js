1e82146f805a99e276ef1831256706b59161f277
Daniel Polk <Danielpolk7@icloud.com>
2023-08-08 20:46:30 -0400

    added modal for creating issues

--- script/script.js
+++
@@ -1,2 +0,0 @@
-// 84f583d3b70337f7a927c7209d583c78
-// issue creation
---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// 84f583d3b70337f7a927c7209d583c78 OpenWeather API key
+
+// Get Geolocation by Name http://api.openweathermap.org/geo/1.0/direct?q={city-name},{state-code},{country-code}&limit={limit}&appid=84f583d3b70337f7a927c7209d583c78
+
+// Get weather by location api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=84f583d3b70337f7a927c7209d583c78
+
+$('#exampleModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
