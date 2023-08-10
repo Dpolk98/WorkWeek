5c3d778b0dcf37a43187fa33cd9fb5fab8ccea03
Daniel Polk <Danielpolk7@icloud.com>
2023-08-10 19:29:40 -0400

    added selects for employee, issue type, and repair time range

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,21 @@
 
 // Get weather by location api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=84f583d3b70337f7a927c7209d583c78
 
-$('#exampleModal').on('shown.bs.modal', function () {
+const value = document.querySelector("#hours");
+const input = document.querySelector("#estimatedTime");
+value.textContent = input.value;
+input.addEventListener("input", (event) => {
+  value.textContent = `Estimated Work Hours: ${event.target.value}`;
+});
+
+$('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+
+// hoursEl.HTML = `Hours estimated: ${sliderEl.value}`;
+
+// sliderEl.onchange = function() {
+//     hoursEl.HTML = `Hours estimated: ${sliderEl.value}`;
+//     console.log(sliderEl.value)
+//     return (sliderEl.value);
+//   }
