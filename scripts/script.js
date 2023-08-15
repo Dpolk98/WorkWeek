754080c919be89bda9908892bd318963cb792f3e
Daniel Polk <Danielpolk7@icloud.com>
2023-08-15 11:34:40 -0400

    added local storage for issues

--- scripts/script.js
+++ scripts/script.js
@@ -8,10 +8,25 @@ const value = document.querySelector("#hours");
 const input = document.querySelector("#estimatedTime");
 var acceptEl = $("#taskAccept");
 var denyEl = $("#taskReject");
+var workDay = document.getElementById("workDay");
+var issueType = document.getElementById("issueType");
+var assignedEmployee = document.getElementById("assignedEmployee");
+var estimatedTime = document.getElementById("estimatedTime")
+var newIssuesEl = document.getElementById("newIssues");
 
 value.textContent = "Estimated Work Hours: 40";
 
 input.addEventListener("input", (event) => {
   value.textContent = `Estimated Work Hours: ${event.target.value}`;
 });
+
+
+acceptEl.on("click", function(){
+    localStorage.setItem(`taskDay`, workDay.value);
+    localStorage.setItem(`taskType`, issueType.value);
+    localStorage.setItem(`taskEmployee`, assignedEmployee.value);
+    localStorage.setItem(`taskTime`, estimatedTime.value);
+    newIssuesEl.append(localStorage.getItem(`taskDay`));
+    console.log(localStorage.getItem(`taskDay`));
+})
 
